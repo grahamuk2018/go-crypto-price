1e84eef7a96b1b19c9f00359b1be147af9d2f28e
grahamuk2018 <42465619+grahamuk2018@users.noreply.github.com>
2021-05-09 10:49:28 +0100

    Create main.go

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"https://github.com/grahamuk2018/go-crypto-price/client"
+)
+
+func main() {
+	fiatCurrency := flag.String(
+		"fiat", "USD,", "The name of the fiat currency you would like to know the price of your crypto in",
+	)
+
+	nameOfCrypto := flag.String(
+		"crypto", "BTC", "Input the name of the CryptoCurrency you would like to know the price of",
+	)
+	flag.Parse()
+
+	crypto, err := client.FetchCrypto(*fiatCurrency, *nameOf)
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println(crypto)
+}
